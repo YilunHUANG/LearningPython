ba434658a52027f292bf03295817429c054eedd6
YilunHUANG <alanhuang.chn@gmail.com>
2015-11-06 11:49:03 +0900

    delete NL

---
+++ ML/LogisticRegression.py
@@ -0,0 +1,14 @@
+'''
+Logistic Regression
+Created on 2015/10/29/
+@author: Alan HUANG
+'''
+import numpy as np
+
+
+def main():
+    X = np.array([])
+    Y = np.array([])
+
+if __name__ == "__main__":
+    main()
